6059df72385cb563be32c1937699009b4f70d17c
MuhammadShariq05 <shariqwrk12023@gmail.com>
2024-07-02 19:37:08 +0530

    Starting

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const name = "Shariq";
-  const [counter, setCounter] = useState(0);
-  return (
-      <>
-      <div>
-        {/* Injected a real object into html */}
-        <h1>Hello {name}</h1>
-      </div>
-      <button onClick={() => setCounter((back) => back - 1)}>-</button>
-      <button>{counter}</button>
-      <button onClick={() => setCounter((fro) => fro + 1)}>+</button>
-      </>
-  )
+  
 }
 
 export default App
